feee4d19f537d6f90a80165e62b9bf70b3753562
unknown <Administrator@PC201406041432.(none)>
2015-02-09 15:45:05 +0800

    add a log util at com.guozha.buy.util package

--- src/com/guozha/buy/util/LogUtil.java
+++ src/com/guozha/buy/util/LogUtil.java
@@ -1,5 +1,39 @@
 package com.guozha.buy.util;
 
-public class LogUtil {
+import android.util.Log;
 
+/**
+ * Logπ§æﬂ¿‡
+ * @author Administrator
+ *
+ */
+public class LogUtil {
+	
+	private static final String TAG = "BUY_TAG";
+	
+	private static final boolean DEBUG = true;
+	
+    public static void d(String TAG, String msg){  
+    	if(!DEBUG) return;
+    	if(TAG == null || msg == null) return;
+        Log.d(TAG, msg);  
+    }  
+      
+    public static void d(String msg){  
+        if(!DEBUG) return;
+        if(msg == null) return;
+        Log.d(TAG, msg);  
+    }  
+      
+    public static void e(String TAG, String msg){  
+    	if(!DEBUG) return;
+    	if(TAG == null || msg == null) return;
+        Log.e(TAG, msg);  
+    }  
+      
+    public static void e(String msg){  
+        if(!DEBUG) return;
+        if(msg == null) return;
+        Log.e(TAG, msg);  
+    }  
 }
