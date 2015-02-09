0bb0d39d9a88cb50f93a55525eab549415723ecb
unknown <Administrator@PC201406041432.(none)>
2015-02-09 16:26:14 +0800

    add mainpage of can slid and auto scroll view

--- src/com/guozha/buy/activity/BaseActivity.java
+++ src/com/guozha/buy/activity/BaseActivity.java
@@ -1,12 +1,10 @@
 package com.guozha.buy.activity;
 
 import android.app.ActionBar;
-import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
 
 import com.guozha.buy.R;
 
