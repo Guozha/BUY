ed92ac9d3e6ece91367ab7d84d775b2089b5b415
unknown <Administrator@PC201406041432.(none)>
2015-02-12 16:01:52 +0800

    modified SplashActivity.java for more useful function

--- src/com/guozha/buy/activity/SplashActivity.java
+++ src/com/guozha/buy/activity/SplashActivity.java
@@ -3,8 +3,10 @@ package com.guozha.buy.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemClock;
 
 import com.guozha.buy.R;
+import com.guozha.buy.util.LogUtil;
 
 /**
  * 闪屏界面
@@ -12,22 +14,61 @@ import com.guozha.buy.R;
  *
  */
 public class SplashActivity extends BaseActivity{
+	
 	private static final int SPLASH_TIME = 2000; //闪屏持续时间
+	private static final int MSG_START_INIT = 0x0001;  //开始初始化
+	private static final int MSG_TURN_MAIN = 0x0002;  //转向MainActivity
+	
+	private long mInitStartTime;
+	
+	private Handler handler = new Handler(){
+		public void handleMessage(android.os.Message msg) {
+			switch (msg.what) {
+			case MSG_START_INIT:
+				mInitStartTime = SystemClock.elapsedRealtime();
+				doInit();
+				int remainDelay =(int)(SPLASH_TIME - 
+						(SystemClock.elapsedRealtime() - mInitStartTime));
+				if(remainDelay <= 0){
+					turnMainActivity();
+				}else{
+					handler.sendEmptyMessageDelayed(MSG_TURN_MAIN, remainDelay);
+				}
+				break;
+			case MSG_TURN_MAIN:
+				turnMainActivity();
+				break;
+			}
+		};
+	};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		setContentView(R.layout.activity_splash);
+	}
+	
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		LogUtil.e("onWindowFocusChanged");
+		handler.sendEmptyMessage(MSG_START_INIT);
+	}
+	
+	/**
+	 * 跳转到主界面
+	 */
+	private void turnMainActivity(){
+		Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+		startActivity(intent);
+		SplashActivity.this.finish();
+	}
+	
+	/**
+	 * 做初始化操作
+	 */
+	private void doInit(){
+		//TODO 做一些业务逻辑（比如加载资源）
 		
-		new Handler().postDelayed(new Runnable() {
-			
-			@Override
-			public void run() {
-				Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-				startActivity(intent);
-				SplashActivity.this.finish();
-			}
-		}, SPLASH_TIME);
 	}
 }
