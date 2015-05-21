5dfe6feb49cbd3ec19358dbc2d7fab598224982e
lixiaoqiang <lxq_xsyu@163.com>
2015-05-21 11:08:25 +0800

    精选里面添加了分页加载相关代码

--- src/com/guozha/buy/fragment/MainTabFragmentMPage.java
+++ src/com/guozha/buy/fragment/MainTabFragmentMPage.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.ListView;
 
 import com.guozha.buy.R;
@@ -13,11 +16,15 @@ import com.guozha.buy.adapter.MPageListAdapter;
 import com.guozha.buy.global.MainPageInitDataManager;
 import com.umeng.analytics.MobclickAgent;
 
-public class MainTabFragmentMPage extends MainTabBaseFragment implements OnClickListener{
+public class MainTabFragmentMPage extends MainTabBaseFragment implements OnScrollListener{
 	
 	private static final String PAGE_NAME = "MainPage";
 	private ListView mListView;
 	private MPageListAdapter mMPageListAdpater;
+	
+	private View mBottomLoadingView; 		//底部刷新视图
+	private TextView mLoadText;
+	private ProgressBar mLoadProgressBar;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater,
@@ -35,6 +42,14 @@ public class MainTabFragmentMPage extends MainTabBaseFragment implements OnClick
 	private void initView(View view){
 		if(view == null) return;
 		mListView = (ListView) view.findViewById(R.id.mpage_list);
+		mBottomLoadingView = getActivity().getLayoutInflater()
+				.inflate(R.layout.list_paging_bottom, null);
+		mBottomLoadingView.setBackgroundColor(getActivity().getResources().getColor(R.color.color_app_base_6));
+		mLoadText = (TextView) mBottomLoadingView.findViewById(R.id.list_paging_bottom_text);
+		mLoadProgressBar = (ProgressBar) 
+				mBottomLoadingView.findViewById(R.id.list_paging_bottom_progressbar);
+		mListView.addFooterView(mBottomLoadingView);
+		
 		mMPageListAdpater = new MPageListAdapter(getActivity());
 		mListView.setAdapter(mMPageListAdpater);
 	}
@@ -60,9 +75,44 @@ public class MainTabFragmentMPage extends MainTabBaseFragment implements OnClick
 		}
 	}
 	
+	//////////////////////////--分页加载相关--/////////////////////////////
+	
+	private int mLastVisibaleIndex; //可见的最大索引
+	private int mMaxDateNum;		//最大条数
+	private int mMaxPageSize;		//最大页数
+	private int mCurrentPage;		//当前页
+
 	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+		if(scrollState == OnScrollListener.SCROLL_STATE_IDLE
+					&& mLastVisibaleIndex == mMPageListAdpater.getCount() 
+					&& mCurrentPage < mMaxPageSize){
+			mLoadProgressBar.setVisibility(View.VISIBLE);
+			mLoadText.setVisibility(View.GONE);
+			loadNextPageData();
+		}
+	}
+
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem,
+			int visibleItemCount, int totalItemCount) {
+		//计算最后可见条目的索引
+		mLastVisibaleIndex = firstVisibleItem + visibleItemCount - 1;
+		
+		//如果现有条目和最大数相等，则移除底部
+		if(totalItemCount == mMaxDateNum + 1){
+			mLoadProgressBar.setVisibility(View.GONE);
+			mLoadText.setVisibility(View.VISIBLE);
+		}
 		
 	}
+	
+	/**
+	 * 加载下一页数据
+	 */
+	private void loadNextPageData(){
+		
+	}
+	
+	//////////////////////////--分页加载END--/////////////////////////////
 }
